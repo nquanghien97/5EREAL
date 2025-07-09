f3a4ac4a21d7301e8d961d0c6dab78e41eda6a19
nquanghien <nquanghien97@gmail.com>
2025-07-09 17:43:57 +0700

    implement: api news

---
+++ app/du-an/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+function DuAn() {
+  return (
+    <main>
+      <section className="">
+        <Image src="/banner-ve-chung-toi.png" alt="banner-ve-chung-toi" width={1831} height={916} className="w-full" />
+      </section>
+
+      <section>
+        
+      </section>
+    </main>
+  )
+}
+
+export default DuAn
--- components/ui/ve-chung-toi/he-sinh-thai-da-linh-vuc.tsx
+++ components/ui/ve-chung-toi/he-sinh-thai-da-linh-vuc.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function HeSinhThaiDaLinhVuc() {
   return (
     <section className="bg-[#fef7e5] py-8">
-      <h2 className="text-3xl md:text-4xl font-bold text-[#0F3E5A] mb-4 text-center">THÀNH TỰU</h2>
+      <h2 className="text-3xl md:text-4xl font-bold text-[#0F3E5A] mb-4 text-center">HỆ SINH THÁI ĐA LĨNH VỰC</h2>
       <div className="container m-auto px-4 mb-8">
         <div className="flex flex-col lg:flex-row">
           <div className="w-full lg:w-1/2 p-4 max-lg:mb-4 relative">
@@ -22,7 +22,7 @@ function HeSinhThaiDaLinhVuc() {
               <Image src="/don-vi-thanh-vien-5.png" alt="don-vi-thanh-vien-5" width={400} height={591} className="w-full" />
             </div>
           </div>
-          <div className="w-full lg:w-1/2 p-4 flex items-center">
+          <div className="w-full lg:w-1/2 p-4 lg:p-16 flex items-center">
             <p className="text-justify text-[#007AA7] text-lg">5E REAL là đơn vị phân phối chuyên sâu, trực thuộc hệ sinh thái Reatimes, gồm: Chủ đầu tư Reatimes Holding, Tạp chí điện tử BĐS Việt Nam Reatimes, Viện Nghiên cứu BĐS Việt Nam (VIPREC) và Hiệp hội BĐS Việt Nam. Kế thừa sức mạnh từ nghiên cứu – truyền thông – kết nối chính sách, 5E mang đến sản phẩm đúng tầm, dịch vụ đúng tâm, tạo nên trải nghiệm giao dịch chuyên nghiệp và bền vững.</p>
           </div>
         </div>
