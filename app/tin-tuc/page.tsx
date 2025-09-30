089cff7ea5425dd9016194278b77787611a9cc40
nquanghien <nquanghien97@gmail.com>
2025-09-30 10:53:27 +0700

    dev

--- app/tin-tuc/page.tsx
+++ app/tin-tuc/page.tsx
@@ -15,13 +15,14 @@ export const metadata: Metadata = {
 
 async function TinTuc({ searchParams }: { searchParams: Promise<{ page: string, pageSize: string }> }) {
   const { page = 1, pageSize = 6 } = await searchParams
-  const response: { data: NewsEntity[], paging: { page: number, pageSize: number, total: number} } = await getNews({ page: Number(page), pageSize: Number(pageSize) })
+  const response: { data: NewsEntity[], paging: { page: number, pageSize: number, total: number } } = await getNews({ page: Number(page), pageSize: Number(pageSize) })
   const responseFirstNews: { data: NewsEntity[] } = await getFirstNews()
   if (response.data.length === 0) {
     return <p className="text-center">Không có dữ liệu</p>
   }
   const firstNews = responseFirstNews.data[0]
   const otherNews = response.data.slice(1)
+
   return (
     <main>
       <section className="mb-8">
@@ -31,27 +32,27 @@ async function TinTuc({ searchParams }: { searchParams: Promise<{ page: string, 
       <section>
         {firstNews ? (
           <div className="container m-auto px-4">
-          <div className="flex flex-col lg:flex-row gap-2 lg:gap-8 lg:h-[400px] mb-4">
-            <div className="lg:w-1/2">
-              <Image src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${firstNews.thumbnailUrl}`} alt={firstNews.title} width={600} height={400} className="object-cover w-full h-full rounded-lg" />
-            </div>
-            <div className="lg:w-1/2 h-full flex flex-col">
-              <h2 className="text-[#0F3E5A] font-bold text-xl mb-2">{firstNews.title}</h2>
-              <p className="text-[#d2a932] font-bold mb-2">{new Date(firstNews.createdAt).toLocaleDateString()}</p>
-              <div dangerouslySetInnerHTML={{ __html: firstNews.content.replace(/<img[^>]*>/g, "") }} className="clamp-html line-clamp-4 text-[#007AA7] mb-4" />
-              <div>
-                <Link href={`tin-tuc/${firstNews.slug}`} className="inline-flex py-2 px-8 text-[#007AA7] bg-gradient-to-r from-[#feedbf] to-[#c29551] rounded-full font-semibold cursor-pointer hover:opacity-80 duration-300">
-                  Xem chi tiết
-                </Link>
+            <div className="flex flex-col lg:flex-row gap-2 lg:gap-8 lg:h-[400px] mb-4">
+              <div className="lg:w-1/2">
+                <Image src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${firstNews.thumbnailUrl}`} alt={firstNews.title} width={600} height={400} className="object-cover w-full h-full rounded-lg" />
+              </div>
+              <div className="lg:w-1/2 h-full flex flex-col">
+                <h2 className="text-[#0F3E5A] font-bold text-xl mb-2">{firstNews.title}</h2>
+                <p className="text-[#d2a932] font-bold mb-2">{new Date(firstNews.createdAt).toLocaleDateString()}</p>
+                <div dangerouslySetInnerHTML={{ __html: firstNews.content.replace(/<img[^>]*>/g, "") }} className="clamp-html line-clamp-4 text-[#007AA7] mb-4" />
+                <div>
+                  <Link href={`tin-tuc/${firstNews.slug}`} className="inline-flex py-2 px-8 text-[#007AA7] bg-gradient-to-r from-[#feedbf] to-[#c29551] rounded-full font-semibold cursor-pointer hover:opacity-80 duration-300">
+                    Xem chi tiết
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         ) : (
           <p className="text-center">Không có dữ liệu</p>
         )}
       </section>
-    
+
       <ListNews data={otherNews} currentPage={Number(page)} total={Number(response.paging.total)} />
     </main>
   )
--- components/news-section/index.tsx
+++ components/news-section/index.tsx
@@ -7,6 +7,9 @@ import React from 'react'
 async function NewsSection() {
   const response = await getNewsByPrisma({ page: 1, pageSize: 4 })
   const result = await response.json() as { data: NewsEntity[] }
+  if (result.data.length === 0) {
+    return <p className="text-center">Không có dữ liệu</p>
+  }
   const firstNews = result.data[0]
   const otherNews = result.data.slice(1)
 
@@ -17,7 +20,8 @@ async function NewsSection() {
           <h2 className="text-3xl md:text-5xl font-bold text-[#0F3E5A] mb-8 text-center">TIN TỨC</h2>
         </div>
         <div className="flex gap-4 flex-col lg:flex-row lg:max-h-[600px]">
-          <div className="relative lg:w-1/2 py-2">
+          {firstNews ? (
+            <div className="relative lg:w-1/2 py-2">
             <Link href={`/tin-tuc/${firstNews.slug}`}>
               <Image src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${firstNews.thumbnailUrl}`} alt={firstNews.title} width={600} height={400} className="object-cover w-full h-full rounded-lg" />
             </Link>
@@ -26,6 +30,9 @@ async function NewsSection() {
               <h3 className="text-xl text-white bg-opacity-70 rounded">{firstNews.title}</h3>
             </div>
           </div>
+          ) : (
+            <p className="text-center">Không có dữ liệu</p>
+          )}
           <div className="lg:w-1/2 flex flex-col">
             {otherNews.map(news => (
               <div className="lg:h-1/3 flex" key={news.id}>
