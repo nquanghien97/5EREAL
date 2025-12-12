55e68fcbc87e2b41761e8f0d4ef7004402b9252e
nquanghien <nquanghien97@gmail.com>
2025-12-12 14:25:18 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
-import { cookies } from "next/headers";
-import { verifyToken } from "@/utils/token";
-import prisma from "@/lib/prisma";
+// import { cookies } from "next/headers";
+// import { verifyToken } from "@/utils/token";
+// import prisma from "@/lib/prisma";
 import { ToastContainer } from 'react-toastify';
 
 const beVietNam = Be_Vietnam_Pro({
@@ -21,34 +21,36 @@ export const metadata: Metadata = {
   description: "Công ty bất động sản 5EREAL",
 };
 
-async function getServerUser() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('access_token')?.value;
-  if (!token) return null;
+// async function getServerUser() {
+//   const cookieStore = await cookies();
+//   const token = cookieStore.get('access_token')?.value;
+//   if (!token) return null;
 
-  const userData = await verifyToken(token);
-  if (!userData) return null;
+//   const userData = await verifyToken(token);
+//   if (!userData) return null;
 
-  const user = await prisma.user.findUnique({
-    where: { id: Number(userData.userId) },
-    select: { id: true, fullName: true, phoneNumber: true, role: true, createdAt: true },
-  });
+//   const user = await prisma.user.findUnique({
+//     where: { id: Number(userData.userId) },
+//     select: { id: true, fullName: true, phoneNumber: true, role: true, createdAt: true },
+//   });
 
-  return user;
-}
+//   return user;
+// }
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const me = await getServerUser()
+  // const me = await getServerUser()
   return (
     <html lang="en" className="scroll-smooth">
       <body
         className={`${beVietNam.className} antialiased`}
       >
-        <Header me={me} />
+        <Header
+          // me={me}
+        />
         {children}
         <Footer />
         <ToastContainer />
